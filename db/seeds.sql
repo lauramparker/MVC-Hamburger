c45b9e433dc93becec8ff787cd195db3a1c4b758
Laura Parker <lauramparker78@gmail.com>
2021-01-23 13:19:24 -0500

    Adding public folder for views

--- db/seeds.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO burgers (name) VALUES ("Burger King Whopper");
-INSERT INTO burgers (name) VALUES ("MacDonald's Big Mac");
-INSERT INTO burgers (name) VALUES ("Wendy's Pretzel Bacon Pub Double");
