c2f65512fb6ed13f2c6f67a704f2d44530171fe7
Laura Parker <lauramparker78@gmail.com>
2021-01-23 13:20:05 -0500

    Updating seed.sql

---
+++ db/seed.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (name) VALUES ("Burger King Whopper");
+INSERT INTO burgers (name) VALUES ("MacDonald's Big Mac");
+INSERT INTO burgers (name) VALUES ("Wendy's Pretzel Bacon Pub Double");
